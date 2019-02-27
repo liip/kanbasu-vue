bb5af91e07cf906d75068fc497852fb0134812f9
Benoît Burgener <benoit.burgener@liip.ch>
2019-02-27 13:45:25 +0100

    Update styleguide config

--- styleguide.config.js
+++ styleguide.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   title: 'Kanbasu Vue',
+  serverPort: 3000,
   defaultExample: false,
   components: 'src/components/**/*.vue',
   require: [path.join(__dirname, 'src/scss/main.scss')],
+  skipComponentsWithoutExample: true,
+  usageMode: 'expand',
 };
