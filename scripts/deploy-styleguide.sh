9d37f5ad3d635d331d5a0db2bb1b92e62b859d3c
Benoît Burgener <benoit.burgener@liip.ch>
2019-02-27 14:06:34 +0100

    Add deployment script

---
+++ scripts/deploy-styleguide.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git stash save
+git checkout -B gh-pages
+git add -f styleguide
+git commit -am "Rebuild styleguide"
+git filter-branch -f --prune-empty --subdirectory-filter styleguide
+git push -f origin gh-pages
+git checkout -
+git stash pop --quiet
